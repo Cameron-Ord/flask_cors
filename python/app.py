7ed397b109aed1fc15b74c8177d7cca4416883fd
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-13 19:40:30 -0600

    running as intended

--- python/app.py
+++ python/app.py
@@ -1,30 +1,44 @@
 #importing
 import json
 from flask import Flask,request, make_response, jsonify
-import dbhelper, api_helper
+import dbhelper, api_helper, dbcreds
+import flask_cors
 app = Flask(__name__)
 
-@app.post('/api/add_artist')
+@app.post('/api/insert_painting')
 #function gets called on api request
-def add_artist():
+def insert_painting():
    try:
       #calls the function in api_helper to loop through the information sent
-         error=api_helper.check_endpoint_info(request.json, ['']) 
+         error=api_helper.check_endpoint_info(request.json, ['artist', 'image_url', 'name']) 
          if(error !=None):
             return make_response(jsonify(error), 400)
          #calls the proceedure to insert sent information into the DB
-         results = dbhelper.run_proceedure('CALL add_artist(?)', [request.json.get('')])
+         results = dbhelper.run_proceedure('CALL insert_painting(?,?,?)', [request.json.get('artist'), request.json.get('image_url'), request.json.get('name')])
          #returns results from db run_proceedure
          if(type(results) == list):
             return make_response(jsonify(results), 200)
          else:
-            return make_response(jsonify('something how gone wrong'), 500)
+            return make_response(jsonify('something has gone wrong'), 500)
 
    except TypeError:
       print('Invalid entry, try again')
       
    except: 
-      print(something went wrong)
+      print('something went wrong')
 
 #running @app
-app.run(debug=True)
+
+if(dbcreds.production_mode == True):
+   print()
+   print('----Running in Production Mode----')
+   print()
+   app.run(debug=True)
+else:
+   from flask_cors import CORS
+   CORS(app)
+   print()
+   print('----Running in Testing Mode----')
+   print()
+   app.run(debug=True)
+   
